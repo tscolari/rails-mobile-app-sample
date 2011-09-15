5ee54bd008a25b601d83a5c75749df435c201dd8
Tiago Scolari <tscolari@gmail.com>
2011-09-15 20:46:16 -0300

    Ups, removing the override that was forcing mobile aways

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   respond_to_mobile_requests :skip_xhr_requests => false
 
-  private
-
-  def is_mobile_request?
-    true
-  end
-
 end
