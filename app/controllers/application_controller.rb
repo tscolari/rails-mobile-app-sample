77606926484f9994c819aa31de815076374e189a
Tiago Scolari <tscolari@gmail.com>
2012-02-03 21:20:03 -0200

    removing the force remote on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   respond_to_mobile_requests :skip_xhr_requests => false
 
-  def is_mobile_request?
-    true
-  end
-
 end
